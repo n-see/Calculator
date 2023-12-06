965f61dce505ea5e04715add17645bbe404b7571
Neo See <neococo123@yahoo.com>
2023-12-05 20:14:00 -0800

    created buttons and linking to js

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+let oneBtn = document.getElementById("oneBtn");
+let twoBtn = document.getElementById("twoBtn");
+let threeBtn = document.getElementById("threeBtn");
+let fourBtn = document.getElementById("fourBtn");
+let fiveBtn = document.getElementById("fiveBtn");
+let sixBtn = document.getElementById("sixBtn");
+let sevenBtn = document.getElementById("sevenBtn");
+let eightBtn = document.getElementById("eightBtn");
+let nineBtn = document.getElementById("nineBtn");
+let zeroBtn = document.getElementById("zeroBtn");
+let plusBtn = document.getElementById("plusBtn");
+let minusBtn = document.getElementById("minusBtn");
+let divideBtn = document.getElementById("divideBtn");
+let multiplyBtn = document.getElementById("multiplyBtn");
+let equalBtn = document.getElementById("equalBtn");
+
+let inputField = document.getElementById("inputField");
+
+let results = document.getElementById("results");
+
